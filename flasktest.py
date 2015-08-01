84ab467c9653316c4a30b6ffb3164df5efa370b3
nrprasad2004 <rama.prasad.nakka@verizon.com>
2015-08-01 23:01:17 +0530

    Update flasktest.py

--- flasktest.py
+++ flasktest.py
@@ -1,5 +1,5 @@
 """Cloud Foundry test"""
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -7,8 +7,9 @@ app = Flask(__name__)
 port = int(os.getenv("VCAP_APP_PORT"))
 
 @app.route('/')
-def hello_world():
-    return 'Hello World2 ! I am running on port ' + str(port)
+def TemperatureMonitor():
+     return render_template('index.html')
+#    return 'Hello World2 ! I am running on port ' + str(port)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
